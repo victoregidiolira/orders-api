587dc279174f7bd64145aadf11da5381ccfc7dc0
Victor <viegidulira@gmail.com>
2026-03-26 12:17:30 -0300

    feat: initial project setup with Spring Boot and AWS dependencies

---
+++ src/test/java/com/vito/orders_api/OrdersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vito.orders_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
